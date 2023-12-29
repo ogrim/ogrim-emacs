cb42f403d9d76b5e46d9003f2e0e19327af3e7b7
Aleksander Skjæveland Larsen <aleksander.larsen@aveva.com>
2023-12-29 12:58:01 +0100

    Fix emacs.bat to work with latest version

--- emacs.bat
+++ emacs.bat
@@ -1,2 +1,17 @@
 @echo off
-"emacsclientw.exe" -na "runemacs.exe" "%1"
+
+IF [%1] == [] (goto LABEL1) else (goto LABEL2)
+
+:LABEL1
+
+emacsclientw.exe --no-wait --alternate-editor runemacs.exe --server-file %HOME%\.emacs.d\server\server "c:\org\work.org"
+
+GOTO EOF
+
+:LABEL2
+
+emacsclientw.exe --no-wait --alternate-editor runemacs.exe --server-file %HOME%\.emacs.d\server\server "%~1"
+
+GOTO EOF
+
+:EOF
